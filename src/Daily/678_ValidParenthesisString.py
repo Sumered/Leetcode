515d8f252c36a7ceb3fa3507a694812340562a3d
Kacper Szufnarowski <kacper.szufnarowski@surveily.com>
2024-04-07 07:05:06 +0200

    Added linear solution

--- src/Daily/678_ValidParenthesisString.py
+++ src/Daily/678_ValidParenthesisString.py
@@ -1,7 +1,33 @@
 from functools import cache
 
+
 class Solution:
     def checkValidString(self, s: str) -> bool:
+        open_parentheses, wildcards = [], []
+
+        for index in range(len(s)):
+            character = s[index]
+            if character == "(":
+                open_parentheses.append(index)
+            elif character == ")":
+                if open_parentheses:
+                    open_parentheses.pop()
+                elif wildcards:
+                    wildcards.pop()
+                else:
+                    return False
+            else:
+                wildcards.append(index)
+
+        while open_parentheses and wildcards:
+            open_index = open_parentheses.pop()
+            wildcard_index = wildcards.pop()
+            if wildcard_index < open_index:
+                return False
+
+        return True and not open_parentheses
+
+    def checkValidString_3dDP(self, s: str) -> bool:
         dp = [[[False for _ in range(len(s) + 1)] for _ in range(len(s) + 1)] for _ in range(len(s) + 1)]
         dp[0][0][0] = True
 
@@ -26,7 +52,7 @@ class Solution:
                             dp[dp_index][left_index + 1][right_index] = True
                             if left_index > right_index:
                                 dp[dp_index][left_index][right_index + 1] = True
-                            
+
         for equal_index in range(len(s)):
             if dp[len(s)][equal_index][equal_index]:
                 return True
